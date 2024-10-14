977e59d3d41527c5420eced5023ce8f6209b3d12
Aybars Manav <aybars.manav@gmail.com>
2024-10-14 19:25:28 +0200

    Implemented Wiener filter

--- motion_blur.m
+++ motion_blur.m
@@ -2,8 +2,8 @@ im = imread("cameraman.tif");
 im  = double(im);
 
 %% Generate Motion Blur Transfer Function
-a = 15;
-b = 15;
+a = 5;
+b = 5;
 T = 1;
 
 u = 0:255; u = (u - 128) / 256;
@@ -55,11 +55,74 @@ subplot(2, 2, 4); imagesc(log(1 + abs(fft_filtered)));
 
 %%
 inverse_filtered_mse = computeMSE(im, real(filtered))
+inverse_filtered_snr = computeSNR_db(im, filtered)
+
+
+%% Add noise as well
+mean = 127.5;
+varience = 1;
+noise = mean + sqrt(varience) * randn(size(im));
+
+noisy_motion_blurred_image = motion_blurred_image + noise;
+
+%% Apply Inverse Filtering
+
+fft_motion_blurred = fftshift(fft2(noisy_motion_blurred_image));
+fft_filtered = fft_motion_blurred ./ H;
+filtered = ifft2(fftshift(fft_filtered), "symmetric");
+
+%% Visualize
+
+figure;
+subplot(2, 2, 1); imagesc(log(1 + abs(fft_motion_blurred)));
+subplot(2, 2, 2); imagesc(log(1 + abs(H)));
+subplot(2, 2, 3); imshow(real(filtered), []);
+subplot(2, 2, 4); imagesc(log(1 + abs(fft_filtered)));
+
+inverse_filtered_noisy_mse = computeMSE(im, real(filtered))
+inverse_filtered_noisy_snr = computeSNR_db(im, filtered)
+
+%% Wiener Filtering of noisy image
+k = 2;
+fft_motion_blurred_noisy =  fftshift(fft2(noisy_motion_blurred_image));
+wiener_transfer_func = (H .* conj(H) ./ (H .* conj(H) + k)) ./ H;
+fft_estimate = wiener_transfer_func .* fft_motion_blurred_noisy;
+wiener_filtered_image = ifft2(fftshift(fft_estimate), "symmetric");
+
+figure;
+subplot(2, 2, 1); imagesc(log(1 + abs(fft_motion_blurred_noisy)));
+subplot(2, 2, 2); imagesc(log(1 + abs(wiener_transfer_func)));
+subplot(2, 2, 3); imshow(real(wiener_filtered_image), []);
+subplot(2, 2, 4); imagesc(log(1 + abs(fft_estimate)));
+
+wiener_filtered_mse = computeMSE(im, real(wiener_filtered_image))
+wiener_filtered_snr = computeSNR_db(im, real(wiener_filtered_image))
+
+
+%% Wiener Filtering of noisy image
+k = 1;
+fft_motion_blurred =  fftshift(fft2(motion_blurred_image));
+wiener_transfer_func = (H .* conj(H) ./ (H .* conj(H) + k)) ./ H;
+fft_estimate = wiener_transfer_func .* fft_motion_blurred;
+wiener_filtered_image = ifft2(fftshift(fft_estimate), "symmetric");
+
+figure;
+subplot(2, 2, 1); imagesc(log(1 + abs(fft_motion_blurred)));
+subplot(2, 2, 2); imagesc(log(1 + abs(wiener_transfer_func)));
+subplot(2, 2, 3); imshow(real(wiener_filtered_image), []);
+subplot(2, 2, 4); imagesc(log(1 + abs(fft_estimate)));
+
+wiener_filtered_mse = computeMSE(im, real(wiener_filtered_image))
+wiener_filtered_snr = computeSNR_db(im, real(wiener_filtered_image))
 
 %% MSE computation
-
 function MSE = computeMSE(im1, im2)
     n_elements = numel(im1);
     diff2 = (im1 - im2).^2;
     MSE = sum(diff2, "all") / n_elements;
+end
+
+function SNR_db = computeSNR_db(original_signal, estimated_signal)
+    SNR = sum(original_signal.^2, "all") / sum( (original_signal - estimated_signal).^2, "all");
+    SNR_db = 10 * log(SNR) / log(10);
 end
