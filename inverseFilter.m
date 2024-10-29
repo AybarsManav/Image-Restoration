acadf43fb73630b8ef6f47a4f5834e324d3ceec1
Defne Eraksoy <defne.eraksoy24@gmail.com>
2024-10-29 15:20:19 +0100

    Move functions to separate files and merge

--- inverseFilter.m
+++ inverseFilter.m
@@ -1,13 +1,16 @@
-function inverse_filtered = inverseFilter(motion_blurred_image, fft_deformation, plot)
-
-fft_motion_blurred = fftshift(fft2(motion_blurred_image));
-fft_filtered = fft_motion_blurred ./ fft_deformation;
-inverse_filtered = real(ifft2(fftshift(fft_filtered), "symmetric"));
+function inverse_filtered = inverseFilter(motion_blurred_image, fft_deformation,var_vec,plot)
+    mean = 0;
+    noise = mean + sqrt(var_vec) * randn(size(motion_blurred_image));
+    motion_blurred_image = motion_blurred_image + noise;
+    H = fft_deformation;
+    fft_motion_blurred = fftshift(fft2(motion_blurred_image));
+    fft_filtered = fft_motion_blurred ./ H;
+    inverse_filtered = real(ifft2(fftshift(fft_filtered), "symmetric"));
 
 if plot    
     figure;
-    subplot(2, 2, 1); imagesc(log(1 + abs(fft_motion_blurred))); title("FFT of Motion Blurred Image");
-    subplot(2, 2, 2); imagesc(log(1 + abs(fft_deformation))); title("FFT of Motion Kernel");
+    subplot(2, 2, 1); imagesc(log(1 + abs(fft_motion_blurred))); title("FFT of Motion Blurred and Noisy Image");
+    subplot(2, 2, 2); imagesc(log(1 + abs(H))); title("FFT of Motion Kernel");
     subplot(2, 2, 3); imagesc(log(1 + abs(fft_filtered))); title("FFT of Filtered Image");
     subplot(2, 2, 4); imshow(inverse_filtered, []); title("Restored Image");
 end
